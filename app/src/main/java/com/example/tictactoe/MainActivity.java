6eff67bba7acbe46558f68119ed24c15f367df20
AhmedRizwan23 <ahmedrixwan@gmail.com>
2024-05-23 18:50:11 +0500

    ..

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -40,55 +40,77 @@ public class MainActivity extends AppCompatActivity {
     public void Check(View view) {
         Button btncurrent = (Button) view;
         System.out.println("btncudddrrent" + btncurrent);
-        count++;
-        if (flag == 0) {
-            btncurrent.setText("x");
-            flag++;
-            System.out.println("flag" + flag);
-        } else {
-            btncurrent.setText("0");
-            flag--;
-            System.out.println("flag" + flag);
-        }
-        if (count > 4) {
-            b1 = btn1.getText().toString();
-            System.out.println("value of b1 is " + b1);
-            b2 = btn2.getText().toString();
-            System.out.println("value of b2 is " + b2);
-            b3 = btn3.getText().toString();
-            System.out.println("value of b3 is " + b3);
-            b4 = btn4.getText().toString();
-            System.out.println("value of b4 is " + b4);
-            b5 = btn5.getText().toString();
-            System.out.println("value of b5 is " + b5);
-            b6 = btn6.getText().toString();
-            System.out.println("value of b6 is " + b6);
-            b7 = btn7.getText().toString();
-            System.out.println("value of b7 is " + b7);
-            b8 = btn8.getText().toString();
-            System.out.println("value of b8 is " + b8);
-            b9 = btn9.getText().toString();
-            System.out.println("value of b9 is " + b9);
-            //conditons
-            if (b1.equals(b2) && b2.equals(b3) && !b1.equals("")) {
-                Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
-
-            } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
-                Toast.makeText(this, "winner is " + b4, Toast.LENGTH_SHORT).show();
-            } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
-                Toast.makeText(this, "winner is " + b7, Toast.LENGTH_SHORT).show();
-            } else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
-                Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
-            } else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
-                Toast.makeText(this, "winner is " + b2, Toast.LENGTH_SHORT).show();
-            } else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
-                Toast.makeText(this, "winner is " + b3, Toast.LENGTH_SHORT).show();
-            } else if (b1.equals(b5) && b5.equals(b9) && !b1.equals("")) {
-                Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
-            } else if (b3.equals(b5) && b5.equals(b7) && !b3.equals("")) {
-                Toast.makeText(this, "winner is " + b3, Toast.LENGTH_SHORT).show();
+        if (btncurrent.getText().toString().equals("")) {
+            count++;
+            if (flag == 0) {
+                btncurrent.setText("x");
+                flag++;
+                System.out.println("flag" + flag);
+            } else {
+                btncurrent.setText("0");
+                flag--;
+                System.out.println("flag" + flag);
             }
+            if (count > 4) {
+                b1 = btn1.getText().toString();
+                System.out.println("value of b1 is " + b1);
+                b2 = btn2.getText().toString();
+                System.out.println("value of b2 is " + b2);
+                b3 = btn3.getText().toString();
+                System.out.println("value of b3 is " + b3);
+                b4 = btn4.getText().toString();
+                System.out.println("value of b4 is " + b4);
+                b5 = btn5.getText().toString();
+                System.out.println("value of b5 is " + b5);
+                b6 = btn6.getText().toString();
+                System.out.println("value of b6 is " + b6);
+                b7 = btn7.getText().toString();
+                System.out.println("value of b7 is " + b7);
+                b8 = btn8.getText().toString();
+                System.out.println("value of b8 is " + b8);
+                b9 = btn9.getText().toString();
+                System.out.println("value of b9 is " + b9);
+                //conditons
+                if (b1.equals(b2) && b2.equals(b3) && !b1.equals("")) {
+                    Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
+                    clearbuttons();
 
+                } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
+                    Toast.makeText(this, "winner is " + b4, Toast.LENGTH_SHORT).show();
+                    clearbuttons();
+                } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
+                    Toast.makeText(this, "winner is " + b7, Toast.LENGTH_SHORT).show();
+                    clearbuttons();
+                } else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
+                    Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
+                    clearbuttons();
+                } else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
+                    Toast.makeText(this, "winner is " + b2, Toast.LENGTH_SHORT).show();
+                    clearbuttons();
+                } else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
+                    Toast.makeText(this, "winner is " + b3, Toast.LENGTH_SHORT).show();
+                    clearbuttons();
+                } else if (b1.equals(b5) && b5.equals(b9) && !b1.equals("")) {
+                    Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
+                    clearbuttons();
+                } else if (b3.equals(b5) && b5.equals(b7) && !b3.equals("")) {
+                    Toast.makeText(this, "winner is " + b3, Toast.LENGTH_SHORT).show();
+                    clearbuttons();
+                }
+            }
         }
+    }
+
+    private void clearbuttons() {
+        btn1.setText("");
+        btn2.setText("");
+        btn3.setText("");
+        btn4.setText("");
+        btn5.setText("");
+        btn6.setText("");
+        btn7.setText("");
+        btn8.setText("");
+        btn9.setText("");
+        flag = 0;
     }
 }
