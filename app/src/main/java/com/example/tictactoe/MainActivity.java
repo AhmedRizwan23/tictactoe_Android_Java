169cf20efd1ffe1be6e7bba65912bbd4a6e9426f
AhmedRizwan23 <ahmedrixwan@gmail.com>
2024-05-23 21:25:38 +0500

    ..

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,24 +3,49 @@ package com.example.tictactoe;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    static TextView player1;
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, resetbutton;
+    TextView player2;
     String b1, b2, b3, b4, b5, b6, b7, b8, b9;
     int flag = 0;
     int count = 0;
+    ArrayList<String> myList = new ArrayList<>();
 
-    @Override
+    public static int countOccurrences(ArrayList<String> list, String element) {
+        int count = 0;
+        for (String item : list) {
+            if (item.equals(element)) {
+                count++;
+            }
+        }
+        return count;
+
+
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
         id_init();
-        
+        resetbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearbuttons();
+
+
+            }
+        });
+
     }
 
     private void id_init() {
@@ -33,6 +58,9 @@ public class MainActivity extends AppCompatActivity {
         btn7 = findViewById(R.id.btn7);
         btn8 = findViewById(R.id.btn8);
         btn9 = findViewById(R.id.btn9);
+        resetbutton = findViewById(R.id.resetbutton);
+        player1 = findViewById(R.id.player1);
+        player2 = findViewById(R.id.player2);
 
     }
 
@@ -74,32 +102,67 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
                     clearbuttons();
 
+                    myList.add(b1);
+                    System.out.println("list is " + myList);
+                    winsloss();
+
                 } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
                     Toast.makeText(this, "winner is " + b4, Toast.LENGTH_SHORT).show();
                     clearbuttons();
+                    myList.add(b4);
+                    winsloss();
                 } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
                     Toast.makeText(this, "winner is " + b7, Toast.LENGTH_SHORT).show();
                     clearbuttons();
+                    myList.add(b7);
+                    winsloss();
                 } else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
                     Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
                     clearbuttons();
+                    myList.add(b1);
+                    winsloss();
                 } else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
                     Toast.makeText(this, "winner is " + b2, Toast.LENGTH_SHORT).show();
                     clearbuttons();
+                    myList.add(b2);
+                    winsloss();
                 } else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
                     Toast.makeText(this, "winner is " + b3, Toast.LENGTH_SHORT).show();
                     clearbuttons();
+                    myList.add(b3);
+                    winsloss();
                 } else if (b1.equals(b5) && b5.equals(b9) && !b1.equals("")) {
                     Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
                     clearbuttons();
+                    myList.add(b1);
+                    winsloss();
                 } else if (b3.equals(b5) && b5.equals(b7) && !b3.equals("")) {
                     Toast.makeText(this, "winner is " + b3, Toast.LENGTH_SHORT).show();
                     clearbuttons();
+                    myList.add(b3);
+                    winsloss();
                 } else if (count == 9) {
                     Toast.makeText(this, "no winner", Toast.LENGTH_SHORT).show();
                     clearbuttons();
+                    winsloss();
                 }
+                System.out.println("list is " + myList);
+                winsloss();
             }
+        }
+    }
+
+    private void winsloss() {
+        int countX = countOccurrences(myList, "x");
+        int countO = countOccurrences(myList, "0");
+        System.out.println("Number of 'x': " + countX);
+        System.out.println("Number of '0': " + countO);
+        if (countX > 0 || countO > 0) {
+            player1.setText("Player X: " + countX);
+            player2.setText("Player O: " + countO);
+        } else {
+            player1.setText("Player X: 0");
+            player2.setText("Player O: 0");
         }
     }
 
@@ -117,5 +180,8 @@ public class MainActivity extends AppCompatActivity {
         btn9.setText("");
         flag = 0;
         count = 0;
+        player1.setText("Player X: 0");
+        player2.setText("Player O: 0");
+
     }
 }
