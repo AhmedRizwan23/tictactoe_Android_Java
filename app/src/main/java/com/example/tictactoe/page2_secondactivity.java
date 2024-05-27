c595d6c54db3dee9f313a7595109464070934c9e
AhmedRizwan23 <ahmedrixwan@gmail.com>
2024-05-27 15:06:50 +0500

    listview and dropdown aka spinner

--- app/src/main/java/com/example/tictactoe/page2_secondactivity.java
+++ app/src/main/java/com/example/tictactoe/page2_secondactivity.java
@@ -2,11 +2,28 @@ package com.example.tictactoe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class page2_secondactivity extends AppCompatActivity {
+
+    ListView listview;
+    Spinner spinner;
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayList<String> arrNames = new ArrayList<>();//Arraylist is not static it can be used when we dont know how many items we will be adding at runtime
+    int[] arrNo = new int[]{1, 2, 3, 4, 5};//this is how simple array is defined it is of fix length
+    ArrayList<String> arrids = new ArrayList<>();
+    ArrayList<String> arrtxtview = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +40,81 @@ public class page2_secondactivity extends AppCompatActivity {
         System.out.println("Student Name: " + studentname);
         System.out.println("Roll No: " + rollno);
         TextView txtstudentinfo = findViewById(R.id.txtstudentinfo);
+        listview = findViewById(R.id.listview);
+        spinner = findViewById(R.id.spinner);
+        autoCompleteTextView = findViewById(R.id.actxtview);
 
         txtstudentinfo.setText(" Roll no" + rollno + "Student Name: " + studentname + "Title: " + title);
         getSupportActionBar().setTitle(title);
+        arrNames.add("Ahmed");
+        arrNames.add("ali");
+        arrNames.add("123");
+        arrNames.add("Ahmed");
+        arrNames.add("ali");
+        arrNames.add("123");
+        arrNames.add("Ahmed");
+        arrNames.add("ali");
+        arrNames.add("123");
+        arrNames.add("ali");
+        arrNames.add("123");
+        arrNames.add("Ahmed");
+        arrNames.add("ali");
+        arrNames.add("123");
+        arrNames.add("Ahmed");
+        arrNames.add("ali");
+        arrNames.add("123");
+        arrNames.add("ali");
+        arrNames.add("123");
+        arrNames.add("Ahmed");
+        arrNames.add("ali");
+        arrNames.add("123");
+        arrNames.add("Ahmed");
+        arrNames.add("ali");
+        arrNames.add("123");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrNames);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Toast.makeText(getApplicationContext(), "You have selected " + arrNames.get(position) + " at position " + position, Toast.LENGTH_SHORT).show();
+                } else if (position == 1) {
+                    Toast.makeText(getApplicationContext(), "You have selected " + arrNames.get(position) + " at position " + position, Toast.LENGTH_SHORT).show();
+                } else if (position == 2) {
+                    Toast.makeText(getApplicationContext(), "You have selected " + arrNames.get(position) + " at position " + position, Toast.LENGTH_SHORT).show();
+
+                } else if (position == 3) {
+                    Toast.makeText(getApplicationContext(), "You have selected " + arrNames.get(position) + " at position " + position, Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+
+
+        });
+        arrids.add("1");
+        arrids.add("2");
+        arrids.add("3");
+        arrids.add("4");
+        arrids.add("5");
+        arrids.add("6");
+
+        ArrayAdapter<String> spinneradapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, arrids);
+
+        spinner.setAdapter(spinneradapter);
+
+        arrtxtview.add("c");
+        arrtxtview.add("pyth");
+        arrtxtview.add("java");
+        arrtxtview.add("js");
+
+        ArrayAdapter<String> autoadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrtxtview);
+        autoCompleteTextView.setAdapter(autoadapter);
+        autoCompleteTextView.setThreshold(3);
+
     }
+
 }
